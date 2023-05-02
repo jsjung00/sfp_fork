f3df655dabd80df019db2f7766dc8526cec5105e
arthur <amackeith@ttic.edu>
2023-05-02 16:05:28 -0500

    updated text to csv file to all folders

--- text_to_csv.py
+++ text_to_csv.py
@@ -9,15 +9,19 @@ SAVE_DIR = osp.join(osp.abspath(osp.expanduser("~")), 'logs/saved_csv')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Take in log text file to convert to csv')
-    parser.add_argument('filename', help='file name (including .txt) of log text file in default_s0 folder')
+    parser.add_argument('filename', help='file name (including .txt) of log text file', nargs='?', default="progress.txt")
+    parser.add_argument('foldername', help='folder name (subfoler of logs) containing text file', nargs='?', default="default_s0")
 
     args = parser.parse_args()
     fname, f_ext = os.path.splitext(args.filename)
-    text_path = osp.join(osp.abspath(osp.expanduser("~")), 'logs', 'default_s0', args.filename)
+    text_path = osp.join(osp.abspath(osp.expanduser("~")), 'logs', args.foldername, args.filename)
     print("text path", text_path)
     read_file = pd.read_csv(text_path)
-    os.makedirs(SAVE_DIR, exist_ok=True) 
-    read_file.to_csv(osp.join(SAVE_DIR, fname + ".csv"), index=False)
+    os.makedirs(osp.join(SAVE_DIR, args.foldername), exist_ok=True) 
+    try:
+        read_file.to_csv(osp.join(SAVE_DIR, args.foldername, fname + ".csv"), index=False)
+    except Error as e:
+        print(e)
  
 
 
